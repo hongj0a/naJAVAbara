bec33661c19a6fec0c71157c127c32f243ec9a7e
OR35 <49738398+OR35@users.noreply.github.com>
2019-07-23 18:05:10 +0900

    sad
    dsad

---
+++ MoreDotori/src/main/java/njb/md/controller/IndexController.java
@@ -0,0 +1,46 @@
+package njb.md.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/main")
+	public String main() {
+		return "main/main";
+	}
+
+	@GetMapping("/noti")
+	public String noti() {
+		return "main/notification";
+	}
+
+	@GetMapping("/book")
+	public String book() {
+		return "cheyeon_member/accountBook";
+	}
+
+	@GetMapping("/book2")
+	public String book2() {
+		return "cheyeon_member/accountBook_month";
+	}
+	
+	@GetMapping("/eList")
+	public String eList() {
+		return "exepertList/exepertList";
+	}
+	@GetMapping("/eListIn")
+	public String eListIn() {
+		return "exepertList/exepertListIn";
+	}
+	@GetMapping("/aList")
+	public String aList() {
+		return "assetManagement/assetList";
+	}
+}
